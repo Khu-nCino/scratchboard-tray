01d458ade4306b6e66be08ffbffdac0d05cc4751
Gabriel Keith <gabriel.keith@ncino.com>
2020-02-28 09:35:44 -0500

    Removed fix path code

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,4 @@
 import { app, ipcMain } from "electron";
-import fixPath from "../common/fixpath";
 import WindowManager from "./WindowManager";
 import TrayManager from "./TrayManager";
 
@@ -9,7 +8,6 @@ const windowManager = new WindowManager();
 const trayManager = new TrayManager(windowManager);
 
 function ready() {
-  fixPath();
   trayManager.activate();
   if (isDevelopment) {
     windowManager.showWindow(0);
